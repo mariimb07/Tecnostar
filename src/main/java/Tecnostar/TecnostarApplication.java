e5bc44aefe778797603a0523c2dd6df75dfc8f86
Mariana Miranda de Brito <rm99576@estudante.fieb.edu.br>
2026-06-10 08:41:25 -0300

    Ambiente de dev

---
+++ src/main/java/Tecnostar/TecnostarApplication.java
@@ -0,0 +1,13 @@
+package Tecnostar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnostarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnostarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tecnostar/TecnostarApplicationTests.java
@@ -0,0 +1,13 @@
+package Tecnostar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnostarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
